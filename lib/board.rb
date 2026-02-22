c0178a932f395cc438e0ea9b6246bc3ae80ec454
Jose Quiceno <josequiceno000@gmail.com>
2026-02-22 17:31:52 -0600

    Create Board class

---
+++ lib/board.rb
@@ -0,0 +1,14 @@
+# Board is a grid 
+class Board
+  def initialize
+    @grid = Array.new(3) { Array.new(3) }
+  end
+
+  def display
+    
+  end
+
+  def update(position, symbol)
+    
+  end
+end
