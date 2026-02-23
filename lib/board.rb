a43246bd8d24d7838db6c10711e3c0ff95870dff
Jose Quiceno <josequiceno000@gmail.com>
2026-02-22 18:30:41 -0600

    Create display method

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,9 @@
 # Constants
 HORIZONTAL_DIVIDER = '─'.freeze
-EMPTY_BLOCK = '║ ║'.freeze
-GRID_SIDE_LENGTH = 9
+EMPTY_BLOCK = '║   ║'.freeze
+GRID_SIDE_LENGTH = 15
 
-# Board is a grid 
+# Board is a grid
 class Board
   def initialize
     @grid = Array.new(3) { Array.new(3) { EMPTY_BLOCK }}
@@ -11,6 +11,7 @@ class Board
 
   def display
     grid = @grid.flatten
+
     puts "\n#{HORIZONTAL_DIVIDER * GRID_SIDE_LENGTH}"
 
     grid.each_with_index do |block, index|
@@ -24,9 +25,11 @@ class Board
   end
 
   def update(position, symbol)
-    
+    @grid[position[0]][position[1]] = "║ #{symbol} ║"
+    display
   end
 end
 
 board = Board.new
 board.display
+board.update([1, 2], "X")
