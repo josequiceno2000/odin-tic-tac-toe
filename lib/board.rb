43e2294254b5837cb8978c490c5d348969fb04f2
Jose Quiceno <josequiceno000@gmail.com>
2026-02-22 18:26:03 -0600

    Refactor display method

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,27 @@
 # Constants
-HORIZONTAL_BORDER = '═'
-HORIZONTAL_DIVIDER = '─'
-VERTICAL_BORDER = '║'
-VERTICAL_DIVIDER = '│'
-EMPTY_BLOCK = '║ ║'
-GRID_HEIGHT = 9
-GRID_LENGTH = 9
+HORIZONTAL_DIVIDER = '─'.freeze
+EMPTY_BLOCK = '║ ║'.freeze
+GRID_SIDE_LENGTH = 9
 
 # Board is a grid 
 class Board
   def initialize
     @grid = Array.new(3) { Array.new(3) { EMPTY_BLOCK }}
-    @block_count = 0
   end
 
   def display
     grid = @grid.flatten
-    puts "\n#{HORIZONTAL_BORDER * GRID_LENGTH}"
+    puts "\n#{HORIZONTAL_DIVIDER * GRID_SIDE_LENGTH}"
 
     grid.each_with_index do |block, index|
-      if index == 8
+      if [2, 5, 8].include?(index)
         puts block
-        puts HORIZONTAL_BORDER * GRID_LENGTH
-      elsif [2, 5].include?(index)
-        puts block
-        puts HORIZONTAL_DIVIDER * GRID_HEIGHT
-        @block_count = 0
+        puts HORIZONTAL_DIVIDER * GRID_SIDE_LENGTH
         next
       end
       print block
-      @block_count += 1
     end
   end
-
 
   def update(position, symbol)
     
